bccf30419f4223d750c12695a33ced3f81f9e49c
llevague <lionel.levagueresse@univ-rennes1.fr>
2013-05-26 11:10:37 +0200

    Restrict direct access to pages

--- cookingapp-web-jsf-servlet/src/main/java/org/esupportail/cookingapp/web/rewrite/RestrictConfiguration.java
+++ cookingapp-web-jsf-servlet/src/main/java/org/esupportail/cookingapp/web/rewrite/RestrictConfiguration.java
@@ -4,54 +4,31 @@ import javax.servlet.ServletContext;
 
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
-import org.ocpsoft.rewrite.context.EvaluationContext;
-import org.ocpsoft.rewrite.event.Rewrite;
-import org.ocpsoft.rewrite.param.Constraint;
+import org.ocpsoft.rewrite.config.Direction;
+import org.ocpsoft.rewrite.servlet.config.DispatchType;
 import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
+import org.ocpsoft.rewrite.servlet.config.Path;
+import org.ocpsoft.rewrite.servlet.config.Substitute;
 
 public class RestrictConfiguration extends HttpConfigurationProvider {
-		
-	public static final String authorizedCharsPattern = "[a-zA-Z0-9/:&?.-=_+]*";
-	
-	public static final Constraint<String> authorizedChars = new Constraint<String>() {
-		@Override
-		public boolean isSatisfiedBy(Rewrite event, EvaluationContext context,
-				String value) {
-			return value.matches(authorizedCharsPattern);
-		}
-	};
-	
+
 	@Override
 	public int priority() {
 		return -1000;
 	}
-
+	
 	@Override
 	public Configuration getConfiguration(final ServletContext context) {
 		return ConfigurationBuilder.begin()
-				
-//				.addRule()
-//	            .when(Direction.isInbound()
-//	                     .andNot(DispatchType.isForward())
-//	                     .and(Path.captureIn("path"))
-//	                     .and(URL.captureIn("url")
-//	                    		 .where("url").constrainedBy(authorizedChars))
-//	                     
-//                          .and(RequestParameter.matchesAll("{name}", "{value}")
-//			                  .where("name").constrainedBy(authorizedChars)
-//			                  .where("value").constrainedBy(authorizedChars)
-//			                  .orNot(RequestParameter.exists("{}")))
-//	            )
-//	            .perform(Forward.to("{path}"))
-	            
+					            
 	            // Block direct access to XHTML files
-//	            .addRule()
-//				.when(Direction.isInbound()
-//						.andNot(DispatchType.isForward())
-//						.and(ResourcesCondition.excluded())
-//					)
-//				.perform(SendStatus.error(404))
-
+               .addRule()
+               .when(Direction.isInbound()
+                        .andNot(DispatchType.isForward())
+                        .and(Path.matches("{*}.xhtml")
+                        		.or(Path.matches("{*}/{*}/")))
+                        .andNot(Path.matches("{*}javax.faces.resource{*}")))
+						.perform(Substitute.with("/404"))
 			;
 	}
 }
